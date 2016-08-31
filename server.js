676bc4fce1fb1ec4d07e2132d8359def67c91e27
Edouard FISCHER <baleinesurseine@users.noreply.github.com>
2016-08-31 22:41:54 +0200

    404 page not found

--- server.js
+++ server.js
@@ -130,7 +130,7 @@ app.use(function (err, req, res, next) {
     return next()
   }
   res.render('404', {
-    title: 'Ressource not found',
+    title: 'Page not found',
     url: req.url
   })
 })
